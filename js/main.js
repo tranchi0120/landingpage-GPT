4784dedf2205fa331b7abd3970de8851c9b6f148
Trần Chí <tranchi20012000@gmail.com>
2021-12-26 23:22:15 +0700

    fix nav-menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-const toggle = document.querySelector(".header-toggle ");
-const menu = document.querySelector(".header-menu");
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
 const activeClass = "is-show";
 toggle.addEventListener("click", function () {
   menu.classList.add(activeClass);
 });
 window.addEventListener("click", function (e) {
-  if (!menu.contains(e.target) && !e.target.matches(".header-toggle")) {
+  if (!menu.contains(e.target) && !e.target.matches(".menu-toggle")) {
     menu.classList.remove(activeClass);
   }
 });
