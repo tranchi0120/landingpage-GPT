743b2ee2601ceea16f3ab8e79d0a7a3ac8f377b2
Trần Chí <tranchi20012000@gmail.com>
2021-12-26 14:02:32 +0700

    menu

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const toggle = document.querySelector(".header-toggle ");
+const menu = document.querySelector(".header-menu");
+const activeClass = "is-show";
+toggle.addEventListener("click", function () {
+  menu.classList.add(activeClass);
+});
+window.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !e.target.matches(".header-toggle")) {
+    menu.classList.remove(activeClass);
+  }
+});
+
