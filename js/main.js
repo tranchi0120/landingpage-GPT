7dd6079383654754ef2e71f5918666376ae772b9
Trần Chí <tranchi20012000@gmail.com>
2021-12-26 23:36:33 +0700

    add close menu-bar

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 const toggle = document.querySelector(".menu-toggle");
+const close = document.querySelector(".menu-close");
 const menu = document.querySelector(".menu");
 const activeClass = "is-show";
 toggle.addEventListener("click", function () {
   menu.classList.add(activeClass);
 });
-window.addEventListener("click", function (e) {
-  if (!menu.contains(e.target) && !e.target.matches(".menu-toggle")) {
-    menu.classList.remove(activeClass);
-  }
+
+close.addEventListener("click", function () {
+  menu.classList.remove(activeClass);
 });
+
+// window.addEventListener("click", function (e) {
+//   if (!menu.contains(e.target) && !e.target.matches(".menu-toggle")) {
+//     menu.classList.remove(activeClass);
+//   }
+// });
 
